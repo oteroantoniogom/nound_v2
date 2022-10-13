26fd79ea657d229a16161a463952961e244c4a9b
Antonio <oteroantoniogomez@gmail.com>
2022-10-13 15:20:35 +0200

    commit por si ak en critpo

--- ep02_gr07/js/account.js
+++ ep02_gr07/js/account.js
@@ -6,9 +6,9 @@ email = document.getElementById("email");
 birthday = document.getElementById("birthday");
 checkbox = document.getElementById("checkbox");
 
-function setCookie(cname,cvalue,exdays) {
+function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
@@ -16,10 +16,10 @@ function setCookie(cname,cvalue,exdays) {
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -30,14 +30,36 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
-  let user = getCookie("username");
-  if (user == uname.value) {
-    alert("El nombre de usuario " + user + "ya existe.");
+  let useruname = getCookie("username");
+  let userpsw = getCookie("userpsw");
+  let userpname = getCookie("userpname");
+  let usersurname = getCookie("usersurname");
+  let useremail = getCookie("email");
+  let userbirthday = getCookie("birthday");
+  let usercheckbox = getCookie("checkbox");
+
+  if (userpsw == uname.value) {
+    alert("El nombre de usuario " + useruname + "ya existe.");
+  }
+  if (userpname == uname.value) {
+    alert("El nombre de usuario " + useruname + "ya existe.");
+  }
+  if (usersurname == uname.value) {
+    alert("El nombre de usuario " + useruname + "ya existe.");
+  }
+  if (useremail == uname.value) {
+    alert("El nombre de usuario " + useruname + "ya existe.");
+  }
+  if (userbirthday == uname.value) {
+    alert("El nombre de usuario " + useruname + "ya existe.");
+  }
+  if (usercheckbox == uname.value) {
+    alert("El nombre de usuario " + useruname + "ya existe.");
   } else {
-     user = uname.value;
-     if (user != "" && user != null) {
-       setCookie("username", user, 365);
-     }
+    useruname = uname.value;
+    if (useruname != "" && useruname != null) {
+      setCookie("username", useruname, 365);
+    }
   }
 }
 
