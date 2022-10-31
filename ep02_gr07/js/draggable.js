8879718e8461e2112cfd9a28636a76ba670ca6c2
Diego <100432074@alumnos.uc3m.es>
2022-10-31 13:27:51 +0100

    library

--- ep02_gr07/js/draggable.js
+++ ep02_gr07/js/draggable.js
@@ -1,1 +1,40 @@
-/*https://www.youtube.com/watch?v=z54suepKiIU*/
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function playList() {
+    var x = document.getElementsByClassName("list");
+    var y = document.getElementById("showList");
+    y.innerHTML = "";
+    for ( var i = 0; i < x.length; i++) {
+        y.innerHTML += x[i].getAttribute("data-path") + "<br>";
+    }
+}
+
+function newPlaylist() {
+    var newlist = document.getElementById("newlist");
+    newlist.style.display = "flex";
+}
+
+function closeList() {
+    var newlist = document.getElementById("newlist");
+    newlist.style.display = "none";
+}
+
+function addPlaylist(){
+    var list = document.getElementById("playlist-list");
+    var input = document.getElementById("nombre");
+    var songs = document.getElementById("showList-container");
+    var songs_clone = songs.cloneNode(true);
+    list.appendChild(songs_clone);
+}
